e7468c2e3a75333fe6aa733765b0440f6b0b1697
Shuqian Ying <ying@archymeta.com>
2016-05-22 01:34:02 -0700

    show webrtc crash

--- renderer.js
+++ renderer.js
@@ -1,3 +1,20 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+'use strict';
+
+const RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection;
+
+
+window.onload = () => {
+    console.log('ok loaded ...');
+    let opts = {
+            optional: [
+                { RtpDataChannels: false },
+                { DtlsSrtpKeyAgreement: true }
+            ]
+        };
+    let iceList = [{ url: 'stun:stun.services.mozilla.com' }];
+    let peerConn = new RTCPeerConnection({ iceServers: iceList }, opts);
+    peerConn.createDataChannel('test');
+};
